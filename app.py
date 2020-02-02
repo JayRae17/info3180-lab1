72ce3f7d4034d164b4df18912fce80bade3f4986
EC2 Default User <ec2-user@ip-172-31-80-8.ec2.internal>
2020-02-02 01:11:27 +0000

    created my first Flask app

--- app.py
+++ app.py
@@ -19,7 +19,10 @@ app = Flask(__name__)
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+    
+@app.route("/")
 
-
-if __name__ == '__main__':
-    app.run()
+def home():
+    return "My home page"
+if __name__ == "__main__":
+    app.run(debug=True, host= "0.0.0.0",port= 8080)
